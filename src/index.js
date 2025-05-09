31aa7e399aaa133dac949d95fe9385937d36c4d8
Chris Brubaker <chris@postscript.io>
2025-05-09 17:21:55 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; 
+import './index.css';
 import WeeklyMealPlanner from './components/WeeklyMealPlanner';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <WeeklyMealPlanner />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <WeeklyMealPlanner />
+    </React.StrictMode>
+  );
+} else {
+  console.error("❌ No root element found in index.html");
+}
 
 reportWebVitals();
