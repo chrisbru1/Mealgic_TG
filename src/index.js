0b71db5954fc1cf2434b290d16346b559bb3c91e
Chris Brubaker <chris@postscript.io>
2025-05-09 17:14:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; 
-import WeeklyMealPlanner from './components/WeeklyMealPlanner.jsx'; 
-import CurrentWeekView from './components/CurrentWeekView.jsx';
-import MealContext from './components/MealContext.jsx';
-import CommunityRecipes from './components/CommunityRecipes.jsx';
+import WeeklyMealPlanner from './components/WeeklyMealPlanner';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <WeeklyMealPlanner />  {/* <--  Render your main component here */}
+    <WeeklyMealPlanner />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a
-// function to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: Measure performance (can be disabled in production)
 reportWebVitals();
