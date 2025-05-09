48e4b39b78eb2fd1c98efaba71e0283184c3d19c
Chris Brubaker <chris@postscript.io>
2025-05-09 17:52:50 -0500

    Update CurrentWeekView.jsx

--- src/components/CurrentWeekView.jsx
+++ src/components/CurrentWeekView.jsx
@@ -9,13 +9,16 @@ const CurrentWeekView = () => {
   if (error) return <p className="text-red-500">{error}</p>;
 
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-4">
       {meals.map((meal, index) => (
         <Card key={index} className="bg-gray-700 rounded-lg shadow-lg border border-yellow-500 mb-2">
           <CardContent>
-            <div className='flex justify-between'>
+            <div className='flex justify-between items-center'>
               <h2 className='text-xl font-bold text-yellow-400'>{meal.name}</h2>
             </div>
+            {meal.image && (
+              <img src={meal.image} alt={meal.name} className="w-full h-40 object-cover rounded-md mb-2" />
+            )}
             <p className='italic text-sm mb-2'>{meal.description}</p>
             {meal.link && (
               <a href={meal.link} target="_blank" rel="noopener noreferrer" className="text-yellow-300 underline">
