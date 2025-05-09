bb3e81e64232189275716e730421099156b82698
Chris Brubaker <chris@postscript.io>
2025-05-09 17:17:49 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import WeeklyMealPlanner from './components/WeeklyMealPlanner';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <WeeklyMealPlanner />
   </React.StrictMode>
 );
 
-// Optional: Measure performance (can be disabled in production)
 reportWebVitals();
