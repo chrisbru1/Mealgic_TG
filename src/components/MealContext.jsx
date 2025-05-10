7fa53b0dc5817e434b08ef67e4ac517c698eb419
Chris Brubaker <chris@postscript.io>
2025-05-09 20:58:37 -0500

    Update MealContext.jsx

--- src/components/MealContext.jsx
+++ src/components/MealContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 const MealContext = createContext();
